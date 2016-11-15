08854384845c42bbae42846c280520587fc32734
Eleanora Wilcox <milowilcox@hanovernorwichschools.org>
2016-11-15 14:56:50 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="Tofu Dad message board">
+		<meta name="keywords" content="">
+		<link rel="icon" href="/photos/favicon.ico" type="image/x-icon" />
+		<link rel="shortcut icon" href="/photos/favicon.ico" type="image/x-icon" />
+		<title>Tofu Dad</title>
+		<link rel="stylesheet" href="css/base.css">
+	</head>
+  
+	<body>
+		<header>
+			<h1>Tofu Dad</h1>
+		</header>
+	
+		<nav>
+		</nav>
+
+		<main>
+      <iframe src="http://webchat.freenode.net?channels=%23andsoandthen&uio=Mj10cnVlJjQ9dHJ1ZSY5PXRydWUmMTE9Mjc3JjEyPXRydWU2e" width="647" height="400"></iframe>
+		</main>
+
+		<footer>
+		</footer>
+	</body>
+</html>

58f97ab68145677090f043730ccf1130d266cf8e
Eleanora Wilcox <milowilcox@hanovernorwichschools.org>
2016-11-16 11:48:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<meta name="description" content="Tofu Dad message board">
+		<meta name="description" content="Tofu Dad home page">
 		<meta name="keywords" content="">
-		<link rel="icon" href="/photos/favicon.ico" type="image/x-icon" />
-		<link rel="shortcut icon" href="/photos/favicon.ico" type="image/x-icon" />
+		<link rel="icon" href="/content/photos/favicon.ico" type="image/x-icon" />
+		<link rel="shortcut icon" href="/content/photos/favicon.ico" type="image/x-icon" />
 		<title>Tofu Dad</title>
 		<link rel="stylesheet" href="css/base.css">
 	</head>

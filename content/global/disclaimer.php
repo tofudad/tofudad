7f0fc4c1be1d1c001361bc9a03355995fa943006
Eleanora Wilcox <milowilcox@hanovernorwichschools.org>
2016-11-17 10:58:05 -0500

    Update and rename disclaimer.txt to disclaimer.php

---
+++ content/global/disclaimer.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p>By using this site, you cede your right to get angry at us administrators of aforementioned site. There is obviously an apparent anonymity here, let us warn you now THERE IS NO ANONYMITY. If we see people doing shitty things, we will do our best to keep the off the site. In general, we the administrators will attempt to maintain an kind and orderly environment. However, we cannot guarantee that you will not see and/or experience inappropriate behavior. If you find something you consider unacceptable, report it by either emailing one of us administrators or sending a form through the contact page. Anyways, have fun, be safe, and be nice!</p>";
+?>
